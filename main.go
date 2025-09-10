d8c83d10924e7a5732d3f4fcf6c53459c7fd6af0
Emerson <emerson.day@gmail.com>
2025-09-09 20:52:33 -0700

    ignore

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// HandleRequest is our lambda handler
+// lambda handler
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// For now, we're just logging the request path and returning a simple message.
+	// logging the request path and returning a simple message
 	fmt.Printf("Processing request for path: %s\n", request.Path)
 
-	// This is the response that API Gateway expects
+	// the response that API Gateway expects
 	return events.APIGatewayProxyResponse{
 		Body:       `{"message": "Hello from your Go API!"}`,
 		StatusCode: 200,
